3b58ef9e61ed1b7f9445e15a5fd48c659108030f
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-06-08 20:34:30 +0200

    Point 3 (not all thread stop)

--- Jobs.h
+++ Jobs.h
@@ -22,7 +22,12 @@ private:
     std::mutex mutex_queue;
     const int max_queue_size = 10;
 
+    std::atomic<bool> producer_stopped = false;
+
 public:
+
+    void producer_end();
+    bool continue_check();
 
     // inserisce un job in coda in attesa di essere processato, può
     // essere bloccante se la coda dei job è piena
@@ -36,9 +41,10 @@ public:
 template<class T>
 T Jobs<T>::get() {
     std::unique_lock<std::mutex> lock(mutex);
-    while (queue.size() < 1)
+    while (queue.size() < 1){
         cv.wait(lock);
-    std::cout << "get: " << queue.size() << std::endl;
+    }
+//    std::cout << "get: " << queue.size() << std::endl;
     T row = queue.front();
     queue.pop();
     cv_queue.notify_one();
@@ -50,12 +56,25 @@ void Jobs<T>::put(T job){
 //    std::lock_guard<std::mutex> l(mutex);
     std::unique_lock<std::mutex> lock(mutex);
     if(queue.size() >= max_queue_size){
-        std::cout << "Locked put: " << queue.size() << std::endl;
+//        std::cout << "Locked put: " << queue.size() << std::endl;
         cv_queue.wait(lock);
     }
-    std::cout << "put: " << queue.size() << std::endl;
+//    std::cout << "put: " << queue.size() << std::endl;
     queue.push(job);
     cv.notify_one();
 }
+
+template<class T>
+void Jobs<T>::producer_end() {
+    std::unique_lock<std::mutex> lock(mutex);
+    producer_stopped = true;
+}
+
+template<class T>
+bool Jobs<T>::continue_check() {
+    std::unique_lock<std::mutex> lock(mutex);
+    return !(producer_stopped && queue.size() == 0);
+}
+
 
 #endif //PDS_LAB4_JOBS_H
--- main.cpp
+++ main.cpp
@@ -7,38 +7,53 @@
 #include <filesystem>
 #include <fstream>
 
-Jobs<Row> jobs;
+Jobs<Row> linejobs;
+Jobs<std::string> fileJobs;
 
-void producer(std::string directory){
+void producer(const std::string& directory){
     std::string line;
     for(auto& f: std::filesystem::directory_iterator(directory)){
         std::cout << f.path() << " " << f.is_regular_file() << std::endl;
         if(f.is_regular_file()){
-            std::ifstream file(f.path());
-            if(file.is_open()){
-                int line_count = 0;
-                while (std::getline(file, line))
-                    jobs.put(Row(f.path().filename(), line_count++, line));
-                file.close();
-            }
+            fileJobs.put(f.path());
         }
     }
-    jobs.put(Row("", -1, ""));
+    std::cout << "Exit thread p" << std::endl;
+
+    fileJobs.producer_end();
 }
 
-void consumer(std::string re){
+void consumer1(){
+    std::string line;
+    std::string file_path;
+    while (fileJobs.continue_check()) {
+        file_path = fileJobs.get();
+        std::ifstream file(file_path);
+        std::cout << "Opening file: " << file_path << std::endl;
+        if (file.is_open()) {
+            int line_count = 0;
+            while (std::getline(file, line))
+                linejobs.put(Row(file_path, line_count++, line));
+            file.close();
+        }
+    }
+    std::cout << "Exit thread c1" << std::endl;
+    linejobs.producer_end();
+}
+
+void consumer2(const std::string& re){
     std::regex regex(re);
     Row row("", 0, "");
-    while (row.getLineNumber()>=0){
-        row = jobs.get();
+    while (linejobs.continue_check()){
+        row = linejobs.get();
 //        std::cout << row.getFileName() << " " << row.getLineNumber() << std::endl;
         if(std::regex_match(row.getLineContent(), regex))
             std::cout << "File: " << row.getFileName() << " Line: " << row.getLineNumber() << " Match: " << row.getLineContent() << std::endl;
     }
 
     // devo aggiungere altri elementi sentinella alla lista perchè un solo valore chiuderebbe un solo thread
-    jobs.put(Row("", -1, ""));
-    std::cout << "Exit thread" << std::endl;
+    linejobs.put(Row("", -1, ""));
+    std::cout << "Exit thread c2" << std::endl;
    }
 
 
@@ -50,9 +65,13 @@ int main() {
     const int threadNumber = 3;
     std::vector<std::thread> threads;
 
-    // lancio i thread dei consumers
+    // lancio i thread dei consumers1
     for(int i=0; i<threadNumber; i++)
-        threads.emplace_back(std::thread(consumer, std::ref(re)));
+        threads.emplace_back(std::thread(consumer1));
+
+    // lancio i thread dei consumers2
+    for(int i=0; i<threadNumber; i++)
+        threads.emplace_back(std::thread(consumer2, std::ref(re)));
 
     producer(directory);
 
